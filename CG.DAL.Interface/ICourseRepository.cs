dabb7b12a303e067d3056ea692b8b7aec1e0c6ce
Hoang054 <you@example.com>
2020-10-22 09:56:20 +0700

    Add project files.

---
+++ CG.DAL.Interface/ICourseRepository.cs
@@ -0,0 +1,12 @@
+﻿using CG.Domain.Response.Course;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CG.DAL.Interface
+{
+    public interface ICourseRepository
+    {
+        IEnumerable<CourseView> Gets();
+    }
+}
